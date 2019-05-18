9dbdfcf7944fa97e3c0480de2fbc51ad5d28eb57
alpheonix <arthurblanc98@gmail.com>
2019-05-18 16:29:55 +0200

    display values

--- app/src/main/java/com/example/starwarskapten/MainActivity.kt
+++ app/src/main/java/com/example/starwarskapten/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.starwarskapten
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
 import android.support.v7.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
+import okhttp3.OkHttpClient
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,31 +20,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //recyclerView_main.setBackgroundColor(Color.BLUE)
-        recyclerView_main.layoutManager= LinearLayoutManager(this)
-        recyclerView_main.adapter = MainAdapter()
-
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        //recyclerView_main.adapter = MainAdapter()
+        println("teeeeeeeeest")
         fetchJson()
     }
 
-     fun fetchJson(){
-        println("fetch")
-         val urlkapten = "https://backup-star-wars.herokuapp.com/trips"
-         val otherurl = "https://api.letsbuildthatapp.com/youtube/home_feed"
-         val mock = "https://demo2421622.mockable.io/starwars"
-         val client = OkHttpClient()
-         val request = Request.Builder().url(mock).build()
+        fun fetchJson() {
+            println("fetch")
+            val urlkapten = "https://backup-star-wars.herokuapp.com/trips"
+            val otherurl = "https://api.letsbuildthatapp.com/youtube/home_feed"
+            val mock = "https://demo2421622.mockable.io/starwars"
+            val client = OkHttpClient()
+            //cette requette ne marche pas erreur 406
+            //val request = Request.Builder().url(urlkapten).addHeader("Content-Type", "application/json").build()
+            val request = Request.Builder().url(mock).addHeader("Content-Type", "application/json").build()
 
 
 
-        client.newCall(request).enqueue(object: Callback{
-            override fun onFailure(call: Call, e: IOException) {
-                println(e)
-            }
 
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body()?.string()
-                println(body)
-            }
-        })
+
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    println(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body()?.string()
+                    val gson = GsonBuilder().create()
+                    val trips:Array<Trips> = gson.fromJson(body,Array<Trips>::class.java)
+                    runOnUiThread{
+                        recyclerView_main.adapter = MainAdapter(trips)
+                    }
+                }
+            })
+        }
     }
-}
+
+class Trips(val id: Int, val pilot: Pilot,val pick_up: Pick_up,val drop_off: Drop_off)
+
+class Pilot(val name: String,val avatar:String,val rate:Int)
+class Pick_up(val name:String,val picture:String,val date:String)
+class Drop_off(val name:String,val picture:String,val date:String)
+
--- app/src/main/java/com/example/starwarskapten/MainAdapter.kt
+++ app/src/main/java/com/example/starwarskapten/MainAdapter.kt
@@ -4,25 +4,29 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_list.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val trips:Array<Trips>): RecyclerView.Adapter<CustomViewHolder>() {
 
-    val trips = listOf<String>("1","2","3")
 
     override fun getItemCount(): Int {
         return trips.size
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
-        val layoutInflater = LayoutInflater.from(p0?.context)
-        val cellForRow = layoutInflater.inflate(R.layout.item_list,p0,false)
+    override fun onCreateViewHolder(group: ViewGroup, index: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(group?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.item_list,group,false)
         return CustomViewHolder(cellForRow)
     }
 
     override fun onBindViewHolder(handler: CustomViewHolder, postiton: Int) {
         val trips = trips.get(postiton)
-        handler.view.textView_item_name.text=trips
+        handler.view.textView_item_name.text=trips.pilot.name
+        handler.view.textView_item_drop.text=trips.drop_off.name
+        handler.view.textView_item_pick.text=trips.pick_up.name
+
+
     }
 
 }
